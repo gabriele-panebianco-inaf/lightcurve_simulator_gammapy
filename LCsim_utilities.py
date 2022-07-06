5657cbffe0db632e1a7d2e7bf1126f7ad25ad829
gabriele.panebianco <gabriele.panebianco@inaf.it>
2022-07-06 14:56:24 +0200

    Complete initialization function to read YAML, set Loggers, choose transient from catalogue.

--- LCsim_utilities.py
+++ LCsim_utilities.py
@@ -1,9 +1,10 @@
 ###########################################
-#
-#
+#                                         #
+#                                         #
+#                                         #
 ###########################################
 
-############ Imports
+############ Not-gammapy imports
 
 import numpy as np
 
@@ -24,6 +25,7 @@ import yaml
 from yaml import SafeLoader
 from time import time
 
+# Gammapy imports
 from gammapy.data import Observation
 from gammapy.datasets import SpectrumDataset, Datasets
 from gammapy.irf import (
@@ -43,19 +45,101 @@ from gammapy.utils.random import get_random_state
 
 ############ Functions
 
-def Welcome():
+def Welcome(logger):
     """
     Welcome function. This is used to print info about the code on execution.
     """
-    print('Welcome! A proper Welcome must be implemented.\n')
+    logger.info('Welcome! A proper Welcome must be implemented.')
     return None
 
-def Goodbye(execution_time_start):
+def Initialize(logger, Configuration_file_path):
+    """
+    Read the YAML Configuration file into a dictionary.
+    Load the Transient Catalogue and choose one transient.
+    Create the output directory and a log file.
+    
+    Parameters
+    ----------
+    logger : `logging.Logger`
+        Logger from main.
+    Configuration_file_path : str
+        Name of the input YAML file.
+    Returns
+    -------
+    configuration : dict
+        Dictionary with the parameters from the YAML file.
+    transient : `astropy.table.row.Row`
+        Row that contains the selected transient from the catalogue.
+    """
+
+    # Load YAML as a dict
+    logger.info("Loading YAML file...")
+    with open(Configuration_file_path) as f:
+        configuration = yaml.load(f, Loader = SafeLoader)
+
+    # Load Catalogue of transients
+    logger.info("Loading Sources Catalogue...")
+    with fits.open(configuration['Input_Catalogue']) as hdulist:
+        table_catalog = QTable.read(hdulist['CATALOG'])
+        table_catalog.add_index('name')
+    
+    # Choose a burst
+    if configuration['Name_Transient'] is not None:
+        try:
+            transient = table_catalog.iloc[table_catalog['name'] == configuration['Name_Transient']][0]
+            # Note: we are assuming all names are different.
+            # The transient is an astropy.table.row.Row
+            logger.info('Requested transient available.')
+        except IndexError:
+            logger.error(f"Transient {configuration['Name_Transient']} was not found.")
+            raise
+    else:
+        rng = np.random.default_rng(configuration['Random_Seed'])
+        random_index = rng.integers(0, high=len(table_catalog))
+        transient = table_catalog[random_index]
+        logger.info('Transient randomly sampled.')
+    
+    # Create output directory
+    observation_livetime = configuration['Observation_Livetimes'] * u.Unit(configuration['Time_Unit'])
+    energy_unit = u.Unit(configuration['Energy_Unit'])
+    energy_range_reco = configuration['Energy_Range_Reco'] * energy_unit
+
+    output_directory = configuration['Output_Directory']
+    output_directory+= transient['name'] + '/'
+    output_directory+= configuration['Name_Instrument' ] + '_'
+    output_directory+= configuration['Name_Detector'   ] + '_'
+    output_directory+= str(int(observation_livetime.to('ms').value)) + 'ms_'
+    output_directory+= str(int(energy_range_reco.value[0])) + '_'
+    output_directory+= str(int(energy_range_reco.value[1])) + '_'
+    output_directory+= energy_unit.to_string() + '/'
+    if configuration['Output_Run_ID'] is not None:
+        output_directory += configuration['Output_Run_ID']+'/'
+    
+    os.makedirs(os.path.dirname(output_directory), exist_ok=True)
+
+    # Define Logger for file
+    f_handler = logging.FileHandler(output_directory+'file.log', mode='w')
+    f_handler.setLevel(logging.INFO)
+    f_handler.setFormatter(logging.Formatter('%(asctime)s. %(levelname)s: %(message)s'))# -%(name)s
+    logger.addHandler(f_handler)
+
+    logger.info(f"YAML File : {Configuration_file_path}")
+    logger.info(f"Source Catalogue: {configuration['Input_Catalogue']}")
+
+    if configuration['Name_Transient'] is not None:
+        logger.info(f"Requested Transient Name: {transient['name']}")
+    else:
+        logger.info(f"Random Transient Name: {transient['name']}")
+
+    logger.info(f"Output Directory: {output_directory}")
+    
+    return configuration, transient
+
+def Goodbye(logger, execution_time_start):
     """
     Goodbye function. Mostly counts time.
     """
-    print('\nTotal Runtime = ', time()-execution_time_start, '(s).')
-    print('Goodbye!')
+    logger.info(f"Total Runtime =  {np.round(time()-execution_time_start,3)} (s). Goodbye!")
     return 0
 
 
--- Lightcurve_simulator.py
+++ Lightcurve_simulator.py
@@ -11,53 +11,56 @@ def Simulator(configuration):
     """
     # Section 1: define variables for the whole pipeline
     # # Transient Information
-    Name_Transient = configuration['Name_Transient']
-    Name_Instrument= configuration['Name_Instrument']
-    Name_Detector  = configuration['Name_Detector']
+    # Name_Transient = configuration['Name_Transient']
+    # Name_Instrument= configuration['Name_Instrument']
+    # Name_Detector  = configuration['Name_Detector']
 
-    # # IRFs files
-    File_rsp = configuration['Input_rsp']
-    File_arf = configuration['Input_arf']
-    File_rmf = configuration['Input_rmf']
+    # # # IRFs files
+    # File_rsp = configuration['Input_rsp']
+    # File_arf = configuration['Input_arf']
+    # File_rmf = configuration['Input_rmf']
 
-    # Insert exception here
-
-
-    File_bak = configuration['Input_bak']
-    File_Light_Curve = configuration['Input_Light_Curve']
+    # # Insert exception here
 
 
-    # Energy Axes Parameters
-    Energy_interpolation = data['Energy_interpolation']
-    Energy_slice = data['Energy_slice']
-    Energy_unit = u.Unit(data["Energy_unit"])
+    # File_bak = configuration['Input_bak']
+    # File_Light_Curve = configuration['Input_Light_Curve']
+
+
+    # # Energy Axes Parameters
+    # Energy_interpolation = data['Energy_interpolation']
+    # Energy_slice = data['Energy_slice']
+    # Energy_unit = u.Unit(data["Energy_unit"])
     
-    custom_range_energy_reco = tuple(data['Energy_range_reco']) * Energy_unit
-    custom_range_energy_true = tuple(data['Energy_range_true']) * Energy_unit
+    # custom_range_energy_reco = tuple(data['Energy_range_reco']) * Energy_unit
+    # custom_range_energy_true = tuple(data['Energy_range_true']) * Energy_unit
     
     
-    # Observation Parameters
-    Number_of_LightCurves = data['N_light_curves']
-    Time_unit = u.Unit(data['Time_unit'])
+    # # Observation Parameters
+    # Number_of_LightCurves = data['N_light_curves']
+    # Time_unit = u.Unit(data['Time_unit'])
     
-    t_start_obs= data['Observation_time_start'] * Time_unit
-    t_stop_obs = data['Observation_time_stop' ] * Time_unit
-    live_t_obs = data['Observation_livetimes' ] * Time_unit
-    dead_times = data['Observation_deadtimes' ] * Time_unit
+    # t_start_obs= data['Observation_time_start'] * Time_unit
+    # t_stop_obs = data['Observation_time_stop' ] * Time_unit
+    # live_t_obs = data['Observation_livetimes' ] * Time_unit
+    # dead_times = data['Observation_deadtimes' ] * Time_unit
     
-    # Create Output Directory
-    Output_Directory = configuration['Output_Directory']
-    Output_Directory+= Name_Transient + '/'
-    Output_Directory+= Name_Instrument + '_' + Name_Detector + '_'
-    Output_Directory+= str(int(live_t_obs.to('ms').value)) + 'ms_'
-    Output_Directory+= str(int(custom_range_energy_reco.value[0])) + '_'
-    Output_Directory+= str(int(custom_range_energy_reco.value[1])) + '_'
-    Output_Directory+= Energy_unit.to_string()+'/'
+    # # Create Output Directory
+    # Output_Directory = configuration['Output_Directory']
+    # Output_Directory+= Name_Transient + '/'
+    # Output_Directory+= Name_Instrument + '_' + Name_Detector + '_'
+    # Output_Directory+= str(int(live_t_obs.to('ms').value)) + 'ms_'
+    # Output_Directory+= str(int(custom_range_energy_reco.value[0])) + '_'
+    # Output_Directory+= str(int(custom_range_energy_reco.value[1])) + '_'
+    # Output_Directory+= Energy_unit.to_string()+'/'
     
-    if configuration['Output_run_id'] is not None:
-        Output_Directory += configuration['Output_run_id']+'/'
+    # if configuration['Output_run_id'] is not None:
+    #     Output_Directory += configuration['Output_run_id']+'/'
     
-    os.makedirs(os.path.dirname(Output_Directory), exist_ok=True)
+    # os.makedirs(os.path.dirname(Output_Directory), exist_ok=True)
+
+
+    logger.info('Main body')
 
 
 
@@ -67,22 +70,27 @@ def Simulator(configuration):
 
 if __name__ == '__main__':
 
+    # Configure logger
+    logging.basicConfig(
+        level=logging.INFO,
+        format ='%(levelname)s: %(message)s'
+    )
+    logger = logging.getLogger(__name__)
+    
     # Print message with initial info
-    Welcome()
+    Welcome(logger)
 
     # Set Script Arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--configurationfile", help="name of the configuration YAML file")
     args = parser.parse_args()
 
-    # Load Configuration YAML file as a dict
-    with open(args.configurationfile) as f:
-        configuration = yaml.load(f, Loader = SafeLoader)
-    print(configuration)
-
+    # Load Configuration YAML file
+    configuration, transient = Initialize(logger, args.configurationfile)
+    
     # Execute the simulator
     Simulator(configuration)
 
-    # Maybe implement a Goodbye Function
-    Goodbye(EXECUTION_TIME_START)
+    # Goodbye
+    Goodbye(logger, EXECUTION_TIME_START)
 
