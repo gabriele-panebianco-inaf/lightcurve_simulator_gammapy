d551e58ac8fc8584a4d20d1797feac3f3d395e0f
gabriele.panebianco <gabriele.panebianco@inaf.it>
2022-07-08 17:19:34 +0200

    Complete Responses section for scripts. Onto Models now

--- Lightcurve_simulator.py
+++ Lightcurve_simulator.py
@@ -45,6 +45,9 @@ def Simulator(configuration, transient, output_directory):
     # Load the Empirical Light Curve for Comparison
     # -----------------TO DO?
 
+
+
+
     # Section 2: Define the Detector Response
     logger.info(f"Responses for instrument: {configuration['Name_Instrument']}, detector {configuration['Name_Detector']}.\n")
     
@@ -54,31 +57,22 @@ def Simulator(configuration, transient, output_directory):
         Hdu_edisp= "MATRIX"
         File_arf = configuration['Input_arf']
         Hdu_aeff = "SPECRESP"
-
     elif configuration['Name_Instrument']=="GBM":
         File_rmf = configuration['Input_rsp']
         Hdu_edisp= "SPECRESP MATRIX"
     else:
         logger.error(f"Functions for instrument {configuration['Name_Instrument']} not implemented. Accepted: GBM, COSI.")
-        exit() # Sistemare sta cosa
+        exit()
 
 
-    # Read the Detector Response Matrix
-    Detector_Response_Matrix = Define_Response_Matrix_from_RSP(File_rmf,
-                                                                Hdu_edisp,
-                                                                "EBOUNDS",
-                                                                configuration,
-                                                                logger
-                                                                )
-
-    # Define the Reconstructed Energy Axis from EBOUNDS HDU of RMF/RSP
+    # Read and define the Reconstructed Energy Axis from EBOUNDS HDU of RMF/RSP
     axis_energy_reco = Define_Energy_Axis(File_rmf,
                                           "EBOUNDS",
                                           configuration,
                                           logger,
                                           energy_is_true = False
                                          )
-    # Define the True Energy Axis from MATRIX/SPECRESP MATRIX HDU of RMF/RSP
+    # Read and define the True Energy Axis from MATRIX/SPECRESP MATRIX HDU of RMF/RSP
     axis_energy_true = Define_Energy_Axis(File_rmf,
                                           Hdu_edisp,
                                           configuration,
@@ -90,9 +84,17 @@ def Simulator(configuration, transient, output_directory):
     geom = Define_Geometry(pointing, axis_energy_reco, logger)  
 
 
-    # Define the Effective Area as an array 1D (vs Energy True)
+    # Read the Detector Response Matrix into a 2D Astropy Quantity (vs Energy True, Reco)
+    Detector_Response_Matrix = Read_Response_Matrix_from_RSP(File_rmf,
+                                                                Hdu_edisp,
+                                                                "EBOUNDS",
+                                                                configuration,
+                                                                logger
+                                                                )
+
+    # Read the Effective Area into a 1D Astropy Quantity (vs Energy True)
     if configuration['Name_Instrument']=="COSI":
-        aeff_array = Define_Effective_Area_from_ARF(File_arf,
+        aeff_array = Read_Effective_Area_from_ARF(File_arf,
                                                     Hdu_aeff,
                                                     configuration,
                                                     logger
@@ -101,7 +103,7 @@ def Simulator(configuration, transient, output_directory):
         logger.info("Compute Effective Area.")
         aeff_array = np.sum(Detector_Response_Matrix, axis=1)
 
-    # Define the Effective Area as an array 2D (vs Energy True, Offset)
+    # Define the Effective Area as a Gammapy object
     Effective_Area = Compute_Effective_Area_2D(aeff_array,
                                                axis_offset,
                                                axis_energy_true,
@@ -111,10 +113,8 @@ def Simulator(configuration, transient, output_directory):
                                                output_directory
                                               )
 
-        
-
     
-    # Define the Energy Dispersion Matrix as an EDispKernelMap (with an Exposure Map)
+    # Define the Energy Dispersion Matrix as a Gammapy object (with an Exposure Map)
     Energy_Dispersion_Matrix_Map = Compute_Energy_Dispersion_Matrix(Detector_Response_Matrix,
                                                                     axis_energy_true,
                                                                     axis_energy_reco,
@@ -125,6 +125,40 @@ def Simulator(configuration, transient, output_directory):
                                                                     configuration,
                                                                     transient,
                                                                     output_directory)
+
+
+
+    # Load the Background Spectrum into a 1D Astropy Quantity (vs Energy Reco)
+    bak_model = Read_Background_Spectrum(configuration,
+                                       geom,
+                                       logger
+                                      )
+    # Define the Background as a Gammapy Object
+    if configuration['Name_Instrument']=="COSI":
+        axis_energy_reco_bkg = axis_energy_reco
+    elif configuration['Name_Instrument']=="GBM":
+        logger.warning("GBM wants another Reco Energy Axis for the Background, taken from BAK, not RSP.")
+        axis_energy_reco_bkg = Define_Energy_Axis(configuration['Input_bak'],
+                                                  "EBOUNDS",
+                                                  configuration,
+                                                  logger,
+                                                  energy_is_true = False
+                                                 )
+
+    Background = Compute_Background_3D(bak_model,
+                                        axis_energy_reco_bkg,
+                                        axis_fovlon,
+                                        axis_fovlat,
+                                        geom,
+                                        logger,
+                                        configuration,
+                                        transient,
+                                        output_directory
+                                    )
+    
+    # Define the IRFs Dictionary
+    IRFs = {'aeff' : Effective_Area, 'bkg'  : Background}
+    
     
 
     #
